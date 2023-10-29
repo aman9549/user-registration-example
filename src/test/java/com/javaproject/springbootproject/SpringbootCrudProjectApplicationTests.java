275d05d4525206ac6d651d7571501c969fc5e95b
aman9549 <134130622+aman9549@users.noreply.github.com>
2023-10-29 21:29:46 +0530

    first commit

---
+++ src/test/java/com/javaproject/springbootproject/SpringbootCrudProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaproject.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
